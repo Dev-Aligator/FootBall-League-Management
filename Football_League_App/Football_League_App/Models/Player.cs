f3ea0658b91e6b4d943ddac600d93eb0b18f12ed
duyngo0605 <duydaik03@gmail.com>
2023-06-11 09:53:24 +0700

    cho phép null một số thuộc tính model player

--- Football_League_App/Football_League_App/Models/Player.cs
+++ Football_League_App/Football_League_App/Models/Player.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace Football_League_App.Models;
 
@@ -7,30 +12,46 @@ public partial class Player
 {
     public int Id { get; set; }
 
+    [Key]
+    [DisplayName("Mã Cầu Thủ")]
     public string MaCt { get; set; } = null!;
 
+    [Required]
+    [DisplayName("Tên Cầu Thủ")]
     public string TenCt { get; set; } = null!;
 
-    public int LoaiCt { get; set; }
+    [Required]
+    [DisplayName("Tên Cầu Thủ")]
+    public int? LoaiCt { get; set; }
 
-    public string? QuocTich { get; set; }
+    [DisplayName("Quốc Tịch")]
+    public string QuocTich { get; set; }
 
-    public decimal? Luong { get; set; }
+    [DisplayName("Lương")]
+    public int Luong { get; set; }
 
-    public int? ChieuCao { get; set; }
+    [DisplayName("Chiều Cao")]
+    public int ChieuCao { get; set; }
 
-    public int? CanNang { get; set; }
+    [DisplayName("Cân Nặng")]
+    public int CanNang { get; set; }
 
-    public string? ViTriChinh { get; set; }
+    [DisplayName("Vị Trí Chính")]
+    public string ViTriChinh { get; set; }
 
-    public string? ViTriPhu { get; set; }
+    [DisplayName("Vị Trí Phụ")]
+    public string ViTriPhu { get; set; }
 
-    public DateTime? NgaySinh { get; set; }
+    [DisplayName("Ngày Sinh")]
+    public DateTime NgaySinh { get; set; }
 
-    public int? SoAo { get; set; }
+    [DisplayName("Số Áo")]
+    public int SoAo { get; set; }
 
-    public string? ChanThuan { get; set; }
+    [DisplayName("Chân Thuận")]
+    public string ChanThuan { get; set; }
 
+    [DisplayName("Mã CLB")]
     public string MaClb { get; set; } = null!;
 
     public virtual ICollection<Club> Clubs { get; set; } = new List<Club>();
